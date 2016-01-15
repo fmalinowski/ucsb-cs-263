af9bc787054ebbfd6351c87d485aed3da616c9d0
Aviral Takkar <aviraltakkar@gmail.com>
2016-01-14 22:25:21 -0800

    task queue working. (item 5 assignment 3)

---
+++ base_project/base/src/main/java/cs263w16/Worker.java
@@ -0,0 +1,18 @@
+package cs263w16;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.memcache.*;
+
+// The Worker servlet should be mapped to the "/worker" URL.
+public class Worker extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String key = request.getParameter("keyname");
+        // Do something with key.
+    }
+}
