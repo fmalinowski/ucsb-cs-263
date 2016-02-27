eb2a72547b6990e5f9d301af8465c9d5fc6f759e
Aviral Takkar <aviraltakkar@gmail.com>
2016-02-27 15:39:08 -0800

    expose REST APIs for background tasks

---
+++ goplaces/src/main/java/goplaces/apis/WaypointsReviewAPI.java
@@ -0,0 +1,33 @@
+package goplaces.apis;
+
+import com.google.appengine.api.taskqueue.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * Mapped to /enqueue_waypointsreview_task.
+ * This API supports POST requests to add WaypointsReview background task to the default queue.
+ *
+ * @author Aviral Takkar
+ *
+ * TODO Document the curl command to access API using POST request.
+ */
+public class WaypointsReviewAPI extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            String place_ids = request.getParameter("place_ids");
+            QueueFactory.getDefaultQueue().add(TaskOptions.Builder.withUrl("/waypointsreview").param("places", place_ids));
+            response.sendRedirect("/WaypointsReviewTaskAdded.html");
+        }
+        catch(Exception e ){
+            response.sendRedirect("/TaskAddError.html");
+            System.out.println(e.getMessage());
+        }
+    }
+}
