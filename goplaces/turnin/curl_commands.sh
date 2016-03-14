97a6d2e39fe4b9cbea9ac4148933aeeb419083d6
Aviral Takkar <aviraltakkar@gmail.com>
2016-03-14 16:30:17 -0700

    add more curl commands

--- goplaces/turnin/curl_commands.sh
+++ goplaces/turnin/curl_commands.sh
@@ -18,8 +18,25 @@ curl "http://go-places-ucsb.appspot.com/rest/places/example"
 
 echo
 echo
+echo "Routes Datastore API test"
+echo
+
+
+# Routes Datastore API
+echo "post a route to datatsore"
+echo
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data "{\"origin\":{\"address\":\"santa barbara\"},\"destination\":{\"address\":\"los angeles\"},\"mapJsonAsText\":\"xyz\"}" http://go-places-ucsb.appspot.com/rest/routesapi
+echo
+echo "get the same route from datastore"
+echo
+route_id=5073076857339904
+curl "http://go-places-ucsb.appspot.com/rest/routesapi/$route_id"
+
+echo
+echo
 echo "Places Memcache API test"
 echo
+
 
 # Places Memcache API
 echo "post a place to memcache"
@@ -32,23 +49,9 @@ curl "http://go-places-ucsb.appspot.com/rest/places/example"
 
 echo
 echo
-echo "Routes Datastore API test"
-echo
-
-# Routes Datastore API
-echo "post a route to datatsore"
-echo
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data "{\"origin\":{\"address\":\"santa barbara\"},\"destination\":{\"address\":\"los angeles\"},\"mapJsonAsText\":\"xyz\"}" http://go-places-ucsb.appspot.com/rest/routesapi
-echo
-echo "get the same route from datastore"
-echo
-route_id=5073076857339904	
-curl "http://go-places-ucsb.appspot.com/rest/routesapi/$route_id"
-
-echo
-echo
 echo "Routes Memcache API test"
 echo
+
 
 # Routes Memcache API
 echo "post a route to memcache"
@@ -58,7 +61,7 @@ echo
 echo "get a different route from memcache, if it exists"
 echo
 route_id=5073076857339904
-curl "http://go-places-ucsb.appspot.com/rest/routesmemcache/$route_id"
+curl "http://go-places-ucsb.appspot.com/rest/routesmemcache/route--1849951270638577460"
 
 echo
 echo
@@ -84,4 +87,5 @@ echo
 echo "post a query to fire off a task to find potential waypoints on a route"
 echo
 route_id=5073076857339904
-curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: e21b5c97-cddf-1ae6-c2a9-96d7c3eb08de" -d '{"routeID":"$route_id","radius":"10","keywords":["pet park","museum"]}' "http://go-places-ucsb.appspot.com/rest/boxrouteworkerapi"
+curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: e21b5c97-cddf-1ae6-c2a9-96d7c3eb08de" -d '{"routeID":"5073076857339904","radius":"10","keywords":["pet park","museum"]}' "http://go-places-ucsb.appspot.com/rest/boxrouteworkerapi"
+curl -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: 888a79a6-8bad-73cd-df51-c90edb0aba6a" "http://go-places-ucsb.appspot.com/rest/boxrouteworkerapi/5081456606969856"
